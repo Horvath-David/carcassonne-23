14dfceeee245d4b6b2f45cbeff1d52cafcd7df45
LoneHusko <vadmarton06@gmail.com>
2023-04-10 13:27:27 +0200

    music volume can be changed

--- scripts/MainMenu.cs
+++ scripts/MainMenu.cs
@@ -5,13 +5,18 @@ public partial class MainMenu : Control {
     [Export] public AudioStreamPlayer musicPlayer;
     [Export] public Button muteMusicButton;
     [Export] public Button startButton;
+    [Export] public Control musicVolPopUp;
+    [Export] public HSlider musicVolume;
 
-	public override void _Ready() {
+    public int Volume = 0;
+    
+    public override void _Ready() {
         muteMusicButton.ButtonPressed = true;
     }
 	async public void Start() {
+		startButton.Disabled = true;
 		startButton.Text = "Please wait...";
-		for (int i = 0; i < 40; i++) {
+		for (int i = Volume * (-1); i < 40; i++) {
 			musicPlayer.VolumeDb = i * -1;
 			await ToSignal(GetTree().CreateTimer(0.00005f), "timeout");
 		}
@@ -21,14 +26,37 @@ public partial class MainMenu : Control {
 	public void Exit() {
 		GetTree().Quit();
 	}
-	
-	public void MuteMusic(bool button_pressed){
-        if (button_pressed){ 
+	public void MusicPopup(bool button_pressed) {
+		if (button_pressed) {
+			musicVolPopUp.Show();
+		}
+		else {
+			musicVolPopUp.Hide();
+		}
+	}
+    
+	public void MuteMusic(bool button_pressed) {
+		if (button_pressed)
+		{
 			musicPlayer.StreamPaused = false;
-			muteMusicButton.Text = "♫";
-        } else {
+			muteMusicButton.Text = "Stop";
+		}
+		else
+		{
 			musicPlayer.StreamPaused = true;
-			muteMusicButton.Text = "♪"; 
-        }
-    }
+			muteMusicButton.Text = "Play";
+		}
+	}
+
+	public void ChangeMusicVol(bool value_changed) {
+		double vol = musicVolume.Value;
+		GD.Print(vol);
+		if ((float)vol == 0) {
+			musicPlayer.VolumeDb = -40;
+			Volume = -40;
+			return;
+		}
+		musicPlayer.VolumeDb = ((float) vol - 100) / 3;
+		Volume = ((int)vol - 100) / 3;
+	}
 }
