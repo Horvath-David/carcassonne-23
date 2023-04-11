fe6168d766a6fa67c43df6c304e376412347571a
LoneHusko <vadmarton06@gmail.com>
2023-04-11 13:26:26 +0200

    gameplay music fade-out fix

--- scripts/UIManager.cs
+++ scripts/UIManager.cs
@@ -138,7 +138,7 @@ public partial class UIManager : Control {
         gameEnded = true;
         waitLabel.Show();
         for (int i = Volume * (-1); i <= 40; i++) {
-            musicPlayer.VolumeDb = i * (-1);
+            musicPlayer.VolumeDb = i * -1;
             await ToSignal(GetTree().CreateTimer(0.00005f), "timeout");
         }
 
@@ -189,10 +189,13 @@ public partial class UIManager : Control {
 
     public void ChangeMusicVol(bool value_changed) {
         double vol = musicVolume.Value;
+        GD.Print(vol);
         if ((float)vol == 0) {
             musicPlayer.VolumeDb = -40;
+            Volume = -40;
             return;
         }
         musicPlayer.VolumeDb = ((float) vol - 100) / 3;
+        Volume = ((int)vol - 100) / 3;
     }
 }
